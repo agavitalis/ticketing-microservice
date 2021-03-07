0bc3f7f15f873bc7e5470815f3c808bf184cc5df
Ogbonna Vitalis <vogbonna@axxess.com>
2021-03-07 22:11:05 +0100

    configured ingress server for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express()
 
 app.use(json());
 
+app.get('/api/users/currentuser',(req, res)=>{
+    res.send("I arrived there");
+})
+
 app.listen(3000,()=>{
     console.log("Auth is listening to port 3000!")
 })
